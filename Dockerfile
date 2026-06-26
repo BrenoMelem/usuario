02dcd6cb1a385add8494acdf32eca70d14a4ac44
Breno <brenomelem13@gmail.com>
2026-06-25 23:05:27 -0300

    Dockerizando serviço de usuario

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM gradle:8-jdk21-alpine AS BUILD
+WORKDIR /app
+COPY . .
+RUN gradle build --no-daemon
+
+
+FROM eclipse-temurin:21-jre-alpine
+WORKDIR /app
+
+COPY  --from=build /app/build/libs/*.jar  /app/usuario.jar
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "/app/usuario.jar"]
---
+++ src/main/java/com/javanauta/usuario/infraestructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.javanauta.usuario.infraestructure.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+    public UnauthorizedException (String mensagem){
+        super(mensagem);
+    }
+    public UnauthorizedException (String mensagem, Throwable throwable){
+        super (mensagem, throwable);
+    }
+}
